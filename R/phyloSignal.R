efa7ff1adce21d1d9a30d8638df1046155e38f9d
fkeck <francois.keck@gmail.com>
2015-08-31 15:28:31 +0200

    Fixes in doc for check

--- R/phyloSignal.R
+++ R/phyloSignal.R
@@ -3,23 +3,35 @@
 #' Computes phylogenetic signal with different methods
 #'
 #' This function computes phylogenetic signal statistics
-#' (Blomberg's K and K*, Abouheif's Cmean, Moran's I, and Pagel's Lambda) for traits in phylo4d objects.
+#' (Blomberg's K and K*, Abouheif's Cmean, Moran's I, and Pagel's Lambda) for traits in \code{phylo4d} objects.
 #'
-#' @param p4d a phylo4d object (see Details).
+#' @param p4d a \code{phylo4d} object.
 #' @param methods a character vector giving the methods to compute phylogenetic signal (see Details).
-#' @param reps a numeric value. Number of repetitions for the estimation of p.values with randomization.
+#' @param reps an integer. The number of repetitions for the estimation of p.values with randomization.
 #'
-#'@details p4d has to be a phylo4d object as defined in phylobase package. phylo4d objects combine
-#'phylogenetic tree with data. Each data associated with tips is interpreted as a trait.
-#'By default, the \code{methods} argument is set to "all" and all the available methods are used.
-#'The user can specify which method(s) to use. Possible values are
-#'"\code{I}", "\code{Cmean}", "\code{Lambda}", "\code{K}" and "\code{K.star}".
+#'@details p4d must be a \code{phylo4d} object as defined in \pkg{phylobase} package.
+#'By default, the \code{methods} argument is set to "\code{all}" and all the available methods are used.
+#'The user can specify which method(s) to use. Possible methods are
+#'"\code{I}" (Gittleman & Kot 1990), "\code{Cmean}" (Abouheif 1999)", "\code{Lambda}" (Pagel 1999),
+#'"\code{K}" and "\code{K.star}" (Blomberg et al. 2003).
 #'
 #'@return A list of two dataframes with the values of statistics and associated
 #'p.values for each tested trait and method.
 #'
-#'@seealso \code{\link{phyloSimSignal}} .
+#' @author This function is a general wrapper for C++ subroutines.
+#' C++ code is adapted from R functions in Pavoine and Ricotta (2013)
+#' and Revell (2012).
 #'
+#' @references
+#' Abouheif E. (1999) A method for testing the assumption of phylogenetic independence in comparative data. Evolutionary Ecology Research 1, 895–909.
+#' Blomberg S.P., Garland Jr T. & Ives A.R. (2003) Testing for phylogenetic signal in comparative data: behavioral traits are more labile. Evolution 57, 717–745.
+#' Gittleman J.L. & Kot M. (1990) Adaptation: Statistics and a null model for estimating phylogenetic effects. Systematic Biology 39, 227–241.
+#' Pagel M. (1999) Inferring the historical patterns of biological evolution. Nature 401, 877–884.
+#' Revell L.J. (2012) phytools: an R package for phylogenetic comparative biology (and other things). Methods in Ecology and Evolution 3, 217–223.
+#' Pavoine S. & Ricotta C. (2013) Testing for Phylogenetic Signal in Biological Traits: The Ubiquity of Cross-Product Statistics. Evolution 67, 828–840.
+#'
+#'@seealso \code{\link{phyloSimSignal}} .
+#'@rdname phylosignalStats
 #'@export
 phyloSignal <- function(p4d, methods = c("all", "I", "Cmean", "Lambda", "K", "K.star"), reps = 999){
   methods <- match.arg(methods, several.ok = TRUE)
@@ -66,6 +78,7 @@ phyloSignal <- function(p4d, methods = c("all", "I", "Cmean", "Lambda", "K", "K.
       tmp <- apply(X, 2, lambdaTest, vcv = VCV)
       res$stat$Lambda <- unlist(sapply(tmp, "[", 1))
       res$pvalue$Lambda <- unlist(sapply(tmp, "[", 2))
+      res$pvalue$Lambda <- ifelse(res$pvalue$Lambda < 1/(reps + 1), 1/(reps + 1), res$pvalue$Lambda)
     }
   }
   
@@ -75,10 +88,14 @@ phyloSignal <- function(p4d, methods = c("all", "I", "Cmean", "Lambda", "K", "K.
 }
 
 
-
-
-
-#Force un retour sur R pour l'optimisation... Essayer d'implémenter ça direct en c++ avec la lib GSL.
+#' Test Pagel's Lambda
+#' Optimize Pagel's Lambda and do a likelihood ratio test.
+#' 
+#' @param x a vector of numeric data.
+#' @param vcv the phylogenetic variance-covariance matrix.
+#' 
+#' @details The optimization process is currently performed in R.
+#' Could be interesting to do this in Cpp and merge with code{pagelLogLik}.
 lambdaTest <- function(x, vcv){
   lambda.max <- max(vcv)/max(vcv[lower.tri(vcv)])
   opt <- suppressWarnings(
@@ -97,8 +114,8 @@ lambdaTest <- function(x, vcv){
     }
   } else {
     pvalue <- NA
-  }  
-  return(list(Lambda = Lambda, pvalue = round(pvalue, 3)))
+  }
+  return(list(Lambda = Lambda, pvalue = pvalue))
 }
 
 
