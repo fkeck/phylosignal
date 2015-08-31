20690f24795b11c23b15093628f3c9161d9bb3b6
fkeck <francois.keck@gmail.com>
2015-08-31 15:30:12 +0200

    Update Imports

--- R/phylosignal-package.R
+++ R/phylosignal-package.R
@@ -1,5 +1,15 @@
 #' phylosignal
-#'
+#' 
+#' @useDynLib phylosignal
+#' @import RcppArmadillo
+#' @importFrom Rcpp evalCpp
+#' @exportPattern "^[[:alpha:]]+"
+#' @import adephylo
+#' @import igraph
+#' @import ape
+#' @import phylobase
+#' @importFrom boot boot boot.ci
+#' 
 #' @name phylosignal
 #' @docType package
 NULL
