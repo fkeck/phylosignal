7b662c3009b1ae3e4aa6f5c2fc5b1ce4ed7efbc2
fkeck <francois.keck@gmail.com>
2015-09-04 15:48:09 +0200

    Add the navic dataset

--- R/phylosignal-package.R
+++ R/phylosignal-package.R
@@ -14,3 +14,16 @@
 #' @name phylosignal
 #' @docType package
 NULL
+
+
+#' Phylogeny and pollution sensitivity of diatoms
+#' 
+#' A phylogenetic tree and the pollution sensitivity of 17 diatoms species from the order Naviculales.
+#' 
+#' @format a \code{phylo4d} object.
+#' @source Keck F., Rimet F., Franc A. & Bouchez A. (In press) Phylogenetic signal in diatom ecology: perspectives for aquatic ecosystems biomonitoring. Ecological Applications.
+#' @docType data
+#' @keywords datasets
+#' @name navic
+#' @usage data(navic)
+NULL
