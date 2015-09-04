1f70cb68a122db6f03aac8f06e2858b1d580d8fa
fkeck <francois.keck@gmail.com>
2015-09-04 16:15:35 +0200

    Doc for navic dataset

--- R/phylosignal-package.R
+++ R/phylosignal-package.R
@@ -7,7 +7,6 @@
 #' @import ape
 #' @import RCurl
 #' @importFrom phylobase phylo4d extractTree tdata tipLabels
-#' @importMethodsFrom phylobase as
 #' @importFrom boot boot boot.ci
 #' @importFrom igraph graph.adjacency decompose.graph V graph.density clusters degree plot.igraph
 #' 
