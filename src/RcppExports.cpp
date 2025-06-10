6d6f0427fd32ec261ccba0f53d24de68e87354c6
fkeck <francois.keck@gmail.com>
2025-06-10 11:21:47 +0200

    Rcpp changes

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // moranTest
 List moranTest(NumericVector xr, NumericMatrix Wr, unsigned int reps);
 RcppExport SEXP _phylosignal_moranTest(SEXP xrSEXP, SEXP WrSEXP, SEXP repsSEXP) {
